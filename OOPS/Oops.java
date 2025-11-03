8b14a84d1e8dffada42eb0687856513baf4fd3d6
Sanket <sanketchanagond07@gmail.com>
2025-11-03 21:50:08 +0530

    Constructor

--- OOPS/Oops.java
+++ OOPS/Oops.java
@@ -1,22 +1,46 @@
 public class Oops {
     public static void main(String[] args) {
-        Pen p1 = new Pen();
-        p1.Setcolor("Blue");
-        System.out.println(p1.color);
+        // Pen p1 = new Pen();
+        // p1.Setcolor("Blue");
+        // System.out.println(p1.getColor());
+        // p1.Settip(5);
+        // System.out.println(p1.getTip());
+
+        Student s1 = new Student("Sanket");
+        System.out.println(s1.name);
     }
 
 }
 
 class Pen{
-    String color;
-    int tip;
+    private String color;
+    private int tip;
+
+      String getColor(){
+        return this.color;
+      }
 
     void Setcolor(String newColor){
         color = newColor;
+    }
+
+    int getTip(){
+        return this.tip;
     }
 
     void Settip(int newtip){
         tip = newtip;
     }
 
+}
+
+
+// CONSTRUCTOR
+class Student{
+    String name;
+    // int rollno;
+
+    Student(String name){
+        this.name = name;
+    }
 }
