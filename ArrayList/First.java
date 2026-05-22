e67474b1da60fd66252f4679641aabd8a1632bbc
Sanket <sanketchanagond07@gmail.com>
2026-05-22 23:53:43 +0530

    Started with ArrayList

---
+++ ArrayList/First.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class First {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println(list);
+    }
+}
